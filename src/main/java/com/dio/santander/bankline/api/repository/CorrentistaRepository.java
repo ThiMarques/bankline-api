afacb94fbbb82a4940e9612734c74fd5c58aafec
Thiago Marques <thimarsilva18@gmail.com>
2022-05-31 16:05:24 -0300

    versao final do nosso projeto

--- src/main/java/com/dio/santander/bankline/api/repository/CorrentistaRepository.java
+++ src/main/java/com/dio/santander/bankline/api/repository/CorrentistaRepository.java
@@ -2,7 +2,7 @@ package com.dio.santander.bankline.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.dio.santander.banklineapi.model.Correntista;
+import com.dio.santander.bankline.api.model.Correntista;
 
 public interface CorrentistaRepository extends JpaRepository<Correntista, Integer> {
 
--- src/main/java/com/dio/santander/bankline/api/repository/MovimentacaoRepository.java
+++ src/main/java/com/dio/santander/bankline/api/repository/MovimentacaoRepository.java
@@ -2,7 +2,7 @@ package com.dio.santander.bankline.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.dio.santander.banklineapi.model.Movimentacao;
+import com.dio.santander.bankline.api.model.Movimentacao;
 
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Integer> {
 
